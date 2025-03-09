15bb23f7d311f06d5dafe6af4e721e6003ed85a3
ayzee7 <alex.rozen.5@gmail.com>
2025-03-09 17:37:03 +0300

    Added ordered table and ordered hash table implementation with tests

---
+++ include/array/ord_arr.h
@@ -0,0 +1,114 @@
+#pragma once
+#include <vector>
+#include <utility>
+
+template <class TKey, class TValue>
+class OrderedTable {
+
+	std::vector<std::pair<TKey, TValue>> data;
+
+public:
+
+	struct Iterator {
+		Iterator(std::pair<TKey, TValue>* pointer) : ptr(pointer) {}
+
+		std::pair<TKey, TValue>& operator*() const { return *ptr; }
+		std::pair<TKey, TValue>* operator->() { return ptr; }
+
+		Iterator& operator++() {
+			ptr++;
+			return *this;
+		}
+
+		Iterator operator++(int) {
+			Iterator tmp = *this;
+			++(*this);
+			return tmp;
+		}
+
+		TKey key() { return ptr->first; }
+		TValue& value() { return ptr->second; }
+
+		friend bool operator== (const Iterator& a, const Iterator& b) { return a.ptr == b.ptr; };
+		friend bool operator!= (const Iterator& a, const Iterator& b) { return a.ptr != b.ptr; };
+
+	private:
+		std::pair<TKey, TValue>* ptr;
+	};
+
+	Iterator begin() { 
+		if (!data.size()) return Iterator(nullptr);
+		return Iterator(&data[0]); 
+	}
+	Iterator end() { 
+		if (!data.size()) return Iterator(nullptr);
+		return ++Iterator(&*(data.end() - 1)); 
+	}
+
+	Iterator insert(const TKey& key, const TValue& value) {
+		auto elem = std::make_pair(key, value);
+		int insert_index = 0;
+		if (data.begin() == data.end() || elem.first > data[data.size() - 1].first) {
+			data.push_back(elem);
+			return Iterator(&data[data.size() - 1]);
+		}
+		else {
+			for (int i = 0; i < data.size(); i++) {
+				if (elem.first < data[i].first) {
+					insert_index = i;
+					data.push_back(elem);
+					for (int j = data.size() - 1; j > i; j--) {
+						std::swap(data[j], data[j - 1]);
+					}
+					break;
+				}
+				else if (elem.first == data[i].first) {
+					throw "Cannot overwrite existing key, must delete it first.";
+				}
+			}
+			return Iterator(&data[insert_index]);
+		}
+	}
+
+	Iterator erase(const TKey& key) {
+		int erase_index = -1;
+		for (int i = 0; i < data.size(); i++) {
+			if (key == data[i].first) {
+				erase_index = i;
+				for (int j = i; j < data.size() - 1; j++) {
+					std::swap(data[j], data[j + 1]);
+				}
+				data.pop_back();
+				break;
+			}
+		}
+		if (erase_index == -1 || erase_index == data.size()) {
+			return end();
+		}
+		return Iterator(&data[erase_index]);
+	}
+
+	Iterator find(const TKey& key) {
+		for (int i = 0; i < data.size(); i++) {
+			if (data[i].first == key) {
+				return Iterator(&data[i]);
+			}
+		}
+		return end();
+	}
+
+	size_t size() {
+		return data.size();
+	}
+
+	TValue& operator[](const TKey& key) {
+		auto it = find(key);
+		if (it == end()) throw "Index overflow.";
+		return it.value();
+	}
+
+	bool empty() {
+		return data.size() ? false : true;
+	}
+
+};
